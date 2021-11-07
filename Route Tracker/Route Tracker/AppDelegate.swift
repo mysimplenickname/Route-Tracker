2501f72a8d6d186d2f151ae6cb2face996061893
mysimplenickname <lvmlkv@yandex.ru>
2021-11-07 20:58:33 +0300

    Revert "homework1"
    This reverts commit 30abc50ee37b198398153d1a7cfbd43f75ff1167.

--- Route Tracker/Route Tracker/AppDelegate.swift
+++ Route Tracker/Route Tracker/AppDelegate.swift
@@ -6,15 +6,14 @@
 //
 
 import UIKit
-import GoogleMaps
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
-        GMSServices.provideAPIKey("AIzaSyBeaIxth9K8asqajY1LwDkcLnLV1Zinxbw")
-        
+        // Override point for customization after application launch.
         return true
     }
 
