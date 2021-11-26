63f76ad2aa9c84a5d603225ed0fb51e6175336f6
Leo Malikov <lvmlkv@yandex.ru>
2021-11-26 21:40:32 +0300

    homework7

--- Route Tracker/Route Tracker/App/AppDelegate.swift
+++ Route Tracker/Route Tracker/App/AppDelegate.swift
@@ -7,32 +7,56 @@
 
 import UIKit
 import GoogleMaps
+import UserNotifications
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
-
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        
         GMSServices.provideAPIKey("AIzaSyBeaIxth9K8asqajY1LwDkcLnLV1Zinxbw")
-
+        configureUserNotificationCenter()
+        return true
+    }
+    
+    func configureUserNotificationCenter() {
+        let userNotificationCenter = UNUserNotificationCenter.current()
+        userNotificationCenter.requestAuthorization(options: [.alert, .sound, .badge]) { granted, error in
+            guard granted else {
+                print("Permission not granted")
+                return
+            }
+            self.sendNotificationRequest(content: self.makeNotificationContent(), trigger: self.makeIntervalNotificationTrigger())
+        }
     }
 
+    func makeNotificationContent() -> UNNotificationContent {
+        let content = UNMutableNotificationContent()
+        content.title = "Notification"
+        content.subtitle = "Test"
+        content.body = "Test"
+        content.badge = 1
+        return content
+    }
+    
+    func makeIntervalNotificationTrigger() -> UNNotificationTrigger {
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 20, repeats: false)
+        return trigger
+    }
+    
+    func sendNotificationRequest(content: UNNotificationContent, trigger: UNNotificationTrigger) {
+        let request = UNNotificationRequest(identifier: "notification", content: content, trigger: trigger)
+        let userNotificationCenter = UNUserNotificationCenter.current()
+        userNotificationCenter.add(request) { error in
+            if let error = error {
+                print(error.localizedDescription)
+            }
+        }
+    }
+    
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-
+    
 }
-
